3864a782717e85090c18b5665f23b4fb5c3899ab
Amol <wiratesltd@gmail.com>
2011-11-22 09:20:08 -0800

    changes

---
+++ app/controllers/admin/contacts_controller.rb
@@ -0,0 +1,23 @@
+class Admin::ContactsController < Admin::BaseController
+  
+  before_filter :authenticate_admin!, :except => []
+  
+  def index
+    @contacts = Contact.find(:all, :order => 'id desc').paginate(:per_page => 15, :page => params[:page])
+  end
+  
+  def show
+    @contact = Contact.find(params[:id])
+        
+  end
+  
+  def destroy
+    @contact = Contact.find(params[:id])
+   
+    if @contact.destroy
+      flash[:notice] = "Contact deleted successfully"
+      redirect_to admin_contacts_path
+    end
+  end
+  
+end
